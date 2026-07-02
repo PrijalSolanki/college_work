f6f8cdedb14b3b951a47c47a8e802aec5a835dec
Prijal Solanki <prijalsolanki26032005@gmail.com>
2026-07-02 22:58:07 +0530

    Update Amazonfulfile.py

--- DS_python/Amazonfulfile.py
+++ DS_python/Amazonfulfile.py
@@ -1,2 +1,16 @@
-product=['abc','kbc','xyz',0,0,'def','ghi','jkl']
+product=['abc','kbc','xyz',' ',' ','def','ghi','jkl']
+print(product)
+s=input("Search Data: ")
+if s in product:
+    print("Avaiable product !!")
+else:
+    print("Avaiable Not product !!")
+ui=int(input("Enter index which index u want to update: "))
+p=input("Update product: ")
+product[ui]=p
+print(product)
+if " " in product:
+    print("slot Available !!")
+else:
+    print("Slot cannt Available !!")
 print(product)
